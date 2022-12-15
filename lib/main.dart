7a29e340bb1609bab163bc20cc60079259ded3d4
Pere Solà Claver <pere.solaclaver@gmail.com>
2022-12-15 22:42:20 +0000

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:kombucha_app/pages/ingredients_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kombucha Club',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: const MyHomePage(title: 'Kombucha Toolkit'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    late Widget currentPage;
+
+    switch (selectedIndex) {
+      case 0:
+        currentPage = const IngredientsPage();
+        break;
+      case 1:
+        currentPage = const Text("Receipe!!");
+        break;
+      case 2:
+        currentPage = const Text("Proportions");
+        break;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.restaurant),
+            label: "Ingredients",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book),
+            label: "Receipe",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate),
+            label: "Proportions calculator",
+          ),
+        ],
+        currentIndex: selectedIndex,
+        onTap: (index) => {
+          setState(
+            () {
+              selectedIndex = index;
+            },
+          )
+        },
+      ),
+      body: currentPage,
+    );
+  }
+}
---
+++ lib/pages/ingredients_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class IngredientsPage extends StatelessWidget {
+  const IngredientsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        Ingredient(
+            title: "SCOBY",
+            description: "Stands for Symbiotic Culture of Bacteria and Yeast."),
+        Ingredient(
+            title: "Unflitered apple cider vinegar",
+            description:
+                "Also called \"with the mother\", it is essential to keep the acidity levels high to protect the SCOBY from harmful bacteria."),
+        Ingredient(
+            title: "Tea",
+            description:
+                "It needs to be either pure green or black tea. Flavored tea contains oils that won't work with Kombutcha"),
+        Ingredient(
+            title: "Sugar", description: "Regular Caster or Granulated Sugar."),
+        Ingredient(
+          title: "Water",
+          description:
+              "I have a filter jar of water (Brita style, if you live in Europe, Google that) and I have never had any problem. I have never done it with tap water, it is drinkable where I live. If you live in a place where it is not safe to drink tap water, please use bottled water.",
+        ),
+      ],
+    );
+  }
+}
+
+class Ingredient extends StatelessWidget {
+  final String title;
+  final String description;
+
+  const Ingredient({super.key, required this.title, required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Text(
+                title,
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ),
+            Text(
+              description,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
